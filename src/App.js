1362f1888d32eeabe425f554fe8eda78c3c84839
nakurai <8546764+Nakurai@users.noreply.github.com>
2020-09-19 10:12:10 -0400

    added router to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
 import "./css/App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Banner from "./component/Banner";
+import Home from "./page/Home";
+import Signin from "./page/Signin";
 
 function App() {
   return (
-    <div className="App">
-      <Banner />
-      This is the app component
-    </div>
+    <Router>
+      <div>
+        <Banner />
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/signin">
+            <Signin />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "../css/Banner.css";
-
+import { Link } from "react-router-dom";
 function Banner() {
-  return <div className="Banner">This is the Banner component</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signin">Signin</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Banner;
